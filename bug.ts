1fa9b59d6c533968aa42aa629a0e895eb644ef0b
BugHunter-X <askzenai@gmail.com>
2024-12-05 19:46:07 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,8 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5); // Works fine
+printNumbers(-5); // Unexpected behavior: prints nothing
